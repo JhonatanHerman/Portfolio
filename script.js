282ef7e1d80a450c08223ab40e26ffcdaabe5319
JhowHerman <88865000+JhonatanHerman@users.noreply.github.com>
2023-07-17 11:44:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-function ativaletra(elemento) {
+/*Jhonatan herman 4170970*/
+
+function escrevendo(elemento) {
     const arrTexto = elemento.innerText.split('');
+
     elemento.innerHTML = '';
+
     arrTexto.forEach((letra, i) => {
+
       setTimeout(() => {
         if (letra !== '<' && letra !== '>') {
           elemento.innerHTML += letra;
         }
+
       }, 75 * i);
+
     });
   }
   
   const titulo = document.querySelector('.digitando');
-  ativaletra(titulo);
+  
+  escrevendo(titulo);
 
